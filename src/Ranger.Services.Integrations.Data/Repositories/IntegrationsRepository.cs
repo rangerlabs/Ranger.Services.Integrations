600906c1b755fc933dbc88462664e5ea29607530
Nick Cromwell <cromwellnick@gmail.com>
2020-06-01 16:41:48 -0500

    Call .ToString() in FromSqlRaw()

--- src/Ranger.Services.Integrations.Data/Repositories/IntegrationsRepository.cs
+++ src/Ranger.Services.Integrations.Data/Repositories/IntegrationsRepository.cs
@@ -146,7 +146,7 @@ namespace Ranger.Services.Integrations.Data
             	    	i.inserted_at,
             	    	i.inserted_by
             	    FROM integration_streams i, integration_unique_constraints iuc
-            	    WHERE iuc.project_id = {projectId} AND (i.data ->> 'IntegrationId') = iuc.integration_id::text
+            	    WHERE iuc.project_id = {projectId.ToString()} AND (i.data ->> 'IntegrationId') = iuc.integration_id::text
                )
                SELECT DISTINCT ON (i.stream_id) 
               		i.id,
@@ -186,7 +186,7 @@ namespace Ranger.Services.Integrations.Data
             	    	i.inserted_at,
             	    	i.inserted_by
             	    FROM integration_streams i, integration_unique_constraints iuc
-            	    WHERE iuc.project_id = {projectId} AND (i.data ->> 'IntegrationId') = iuc.integration_id::text
+            	    WHERE iuc.project_id = {projectId.ToString()} AND (i.data ->> 'IntegrationId') = iuc.integration_id::text
                )
                SELECT DISTINCT ON (i.stream_id) 
               		i.id,
