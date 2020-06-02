70e285dc1def84970fa1d2d073674f585a1fc6e3
Nick Cromwell <cromwellnick@gmail.com>
2020-06-02 16:48:04 -0500

    Add signing key unique index per tenant

---
+++ src/Ranger.Services.Integrations.Data/Migrations/20200602214605_AddJsonbSigningKeyIndex.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ranger.Services.Integrations.Data.Migrations
+{
+    public partial class AddJsonbSigningKeyIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql("CREATE UNIQUE INDEX idx_data_signing_key ON integration_streams (tenant_id, (data->>'SigningKey'));");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP INDEX idx_data_signing_key");
+        }
+    }
+}
