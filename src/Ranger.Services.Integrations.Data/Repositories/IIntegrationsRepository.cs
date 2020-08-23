497439a44597ec871e2a02f64f669142faf8696b
Nick Cromwell <cromwellnick@gmail.com>
2020-08-23 00:41:00 -0400

    Include environment in default integrations search

--- src/Ranger.Services.Integrations.Data/Repositories/IIntegrationsRepository.cs
+++ src/Ranger.Services.Integrations.Data/Repositories/IIntegrationsRepository.cs
@@ -13,7 +13,7 @@ namespace Ranger.Services.Integrations.Data
         Task AddIntegrationAsync(string userEmail, string eventName, IEntityIntegration integraiton, IntegrationsEnum integrationType);
         Task<Guid> SoftDeleteAsync(Guid projectId, string userEmail, string name);
         Task<IEnumerable<(IDomainIntegration integration, IntegrationsEnum integrationType, int version)>> GetAllIntegrationsForProject(Guid projectId, CancellationToken cancellationToken = default(CancellationToken));
-        Task<IEnumerable<IDomainIntegration>> GetAllNotDeletedDefaultIntegrationsForProject(Guid projectId, CancellationToken cancellationToken = default(CancellationToken));
+        Task<IEnumerable<IDomainIntegration>> GetAllNotDeletedDefaultIntegrationsByEnvironmentForProject(Guid projectId, EnvironmentEnum environment, CancellationToken cancellationToken = default(CancellationToken));
         Task<IEnumerable<IDomainIntegration>> GetAllNotDeletedIntegrationsByIdsForProject(Guid projectId, IEnumerable<Guid> integrationIds, CancellationToken cancellationToken = default(CancellationToken));
         Task<IEnumerable<IDomainIntegration>> GetAllNotDeletedIntegrationsForProjectIds(IEnumerable<Guid> projectIds, CancellationToken cancellationToken = default(CancellationToken));
         Task<IDomainIntegration> GetNotDeletedIntegrationByIntegrationIdAsync(Guid projectId, Guid integrationId, CancellationToken cancellationToken = default(CancellationToken));
--- src/Ranger.Services.Integrations.Data/Repositories/IntegrationsRepository.cs
+++ src/Ranger.Services.Integrations.Data/Repositories/IntegrationsRepository.cs
@@ -139,7 +139,7 @@ namespace Ranger.Services.Integrations.Data
             return integrationVersionTuples;
         }
 
-        public async Task<IEnumerable<IDomainIntegration>> GetAllNotDeletedDefaultIntegrationsForProject(Guid projectId, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<IEnumerable<IDomainIntegration>> GetAllNotDeletedDefaultIntegrationsByEnvironmentForProject(Guid projectId, EnvironmentEnum environment, CancellationToken cancellationToken = default(CancellationToken))
         {
             var integrationStreams = await this.context.IntegrationStreams
             .FromSqlRaw($@"
@@ -171,7 +171,8 @@ namespace Ranger.Services.Integrations.Data
                         i.inserted_by
                     FROM not_deleted i
                     ORDER BY i.stream_id, i.version DESC) AS integrationstreams
-                WHERE (data ->> 'IsDefault') = 'true'").ToListAsync(cancellationToken);
+                WHERE (data ->> 'IsDefault') = 'true'
+                AND (data ->> 'Environment' = '{environment.ToString()}'").ToListAsync(cancellationToken);
             var integrationVersionTuples = new List<IDomainIntegration>();
             foreach (var integrationStream in integrationStreams)
             {
