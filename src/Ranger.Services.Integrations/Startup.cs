4d6f5150f31d1c618d7195d9f80aa33967bc3cf0
Nick Cromwell <cromwellnick@gmail.com>
2020-09-05 14:36:31 -0400

    update redis package

--- src/Ranger.Services.Integrations/Startup.cs
+++ src/Ranger.Services.Integrations/Startup.cs
@@ -58,7 +58,7 @@ namespace Ranger.Services.Integrations
                 options.UseNpgsql(configuration["cloudSql:ConnectionString"]);
             });
 
-            services.AddRedis(configuration["redis:ConnectionString"]);
+            services.AddRedis(configuration["redis:ConnectionString"], out _);
 
             var identityAuthority = configuration["httpClient:identityAuthority"];
             services.AddPollyPolicyRegistry();
